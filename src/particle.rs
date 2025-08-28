53f2e7e7ff11c830bff468cbf3a066097eeb109e
duncanam <22781288+duncanam@users.noreply.github.com>
2025-08-27 19:23:42 -0700

    Introducing ids to particles

--- src/particle.rs
+++ src/particle.rs
@@ -1,6 +1,4 @@
-use std::{Float::consts::PI, f64::consts::PI};
-
-use rand::random_range;
+use std::f64::consts::PI;
 
 use crate::{math::Math, types::Float};
 
@@ -9,9 +7,10 @@ const MAX_PARTICLE_ANGLE: Float = 2.0 * PI;
 
 /// An individual particle with spatial and rotational state
 struct Particle {
+    id: usize,
     pos_x: Float,
     pos_y: Float,
-    omega: Float,
+    theta: Float,
     phase: Float,
 }
 
@@ -38,24 +37,60 @@ impl Particle {
     }
 
     /// Compute the shortest distance between this particle and another particle
+    #[inline]
     fn compute_euclidean_distance(&self, other: &Self) -> Float {
         // sqrt((x2-x1)^2 - (y2-y1)^2)
         ((other.pos_x - self.pos_x).square() + (other.pos_y - self.pos_y).square()).sqrt()
     }
 
     /// Create a new particle with random initialization
-    fn new(boundary_side_length: Float) -> Self {
+    fn new(id: usize, boundary_side_length: Float) -> Self {
         let pos_x = Self::sample_random_linear_position(boundary_side_length);
         let pos_y = Self::sample_random_linear_position(boundary_side_length);
-        let omega = Self::sample_random_angular_position();
+        let theta = Self::sample_random_angular_position();
         let phase = Self::sample_random_phase();
 
         Self {
+            id,
             pos_x,
             pos_y,
-            omega,
+            theta,
             phase,
         }
+    }
+
+    fn compute_new_theta(&self, particles: &Particles) -> Float {
+        todo!()
+    }
+
+    /// Get the indices of the closest particles in the swarm given a `distance`.
+    ///
+    /// # Notes
+    /// This function is O(n^2) when called externally on a collection, because it iterates over
+    /// each particle checking the distance. If improved performance is required, we can look into
+    /// something like a k-d tree alg that will partition the particles into spatial groups and
+    /// allow for O(log(n)) lookup. See Rust crate `kiddo`.
+    fn compute_idxs_closest(
+        &self,
+        particles: &Particles,
+        distance_threshold: Float,
+    ) -> IdxsNeighborParticles {
+        IdxsNeighborParticles(
+            particles
+                .0
+                // We iterate over each particle...
+                .iter()
+                // ...and ensure we aren't including the target particle itself...
+                .filter(|particle| self.id != particle.id)
+                // ...and then for each particle we compute the euclidean distance between them,
+                // filtering out any particles that are further away than our threshold...
+                .filter(|particle| self.compute_euclidean_distance(particle) < distance_threshold)
+                // ...and then we snag the remaining, filtered indices for particles we know are
+                // within the threshold distance...
+                .map(|particle| particle.id)
+                // ...and place them all together into a collection.
+                .collect(),
+        )
     }
 }
 
@@ -71,39 +106,8 @@ impl Particles {
             // For each particle...
             (0..num_particles)
                 // ...instantiate a random new one...
-                .map(|_| Particle::new(boundary_side_length))
+                .map(|id| Particle::new(id, boundary_side_length))
                 // ...then collect all the particles together into this data structure.
-                .collect(),
-        )
-    }
-
-    /// Get the indices of the closest particles in the swarm given a `distance`.
-    ///
-    /// # Notes
-    /// This function is O(n^2) when called externally on a collection, because it iterates over
-    /// each particle checking the distance. If improved performance is required, we can look into
-    /// something like a k-d tree alg that will partition the particles into spatial groups and
-    /// allow for O(log(n)) lookup. See Rust crate `kiddo`.
-    fn compute_idxs_nearest(&self, idx_target: usize, distance: Float) -> IdxsNeighborParticles {
-        let target_particle = &self.0[idx_target];
-
-        IdxsNeighborParticles(
-            self.0
-                // We iterate over each particle...
-                .iter()
-                // ...and also grab the index of each particle...
-                .enumerate()
-                // ...and ensure we aren't including the target particle itself...
-                .filter(|(idx, _)| *idx != idx_target)
-                // ...and then for each particle we compute the euclidean distance between them,
-                // filtering out any particles that are further away than our threshold...
-                .filter(|(_, particle)| {
-                    target_particle.compute_euclidean_distance(particle) < distance
-                })
-                // ...and then we snag the remaining, filtered indices for particles we know are
-                // within the threshold distance...
-                .map(|(idx, _)| idx)
-                // ...and place them all together into a collection.
                 .collect(),
         )
     }
